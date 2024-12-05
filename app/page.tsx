8b7d2694f92921714cdbe4b07c4f9e994fab9115
Arin7712 <aringawande7712@gmail.com>
2024-12-05 10:47:43 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,12 @@
-"use client";
+import Home from '@/components/Home'
+import React from 'react'
 
-import { AccordionDemo } from "@/components/Accordian";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Services from "@/components/Services";
-import Services2 from "@/components/Services2";
-import TimelineContext from "@/components/TimelineContext";
-import { useEffect, useRef } from "react";
-import dynamic from "next/dynamic"; // Import for dynamic loading
-import "locomotive-scroll/dist/locomotive-scroll.css";
-import Topbar from "@/components/Topbar";
-
-const Home = () => {
-  const scrollRef = useRef(null);
-
-  useEffect(() => {
-    let scroll: any;
-    if (typeof window !== "undefined") {
-      // Dynamically import LocomotiveScroll to ensure it only runs in the browser
-      const LocomotiveScroll = require("locomotive-scroll").default;
-
-      scroll = new LocomotiveScroll({
-        el: scrollRef.current,
-        smooth: true,
-        multiplier: 1.2, // Adjust scroll pace
-      });
-    }
-
-    return () => {
-      if (scroll) scroll.destroy();
-    };
-  }, []);
-
+const page = () => {
   return (
-    <div data-scroll-container ref={scrollRef}>
-      <main data-scroll-section>
-        <Topbar />
-        <Navbar />
-        <Hero />
-        <Services />
-        <Services2 />
-        <TimelineContext />
-        <AccordionDemo />
-        <Footer />
-      </main>
+    <div>
+      <Home/>
     </div>
-  );
-};
+  )
+}
 
-export default dynamic(() => Promise.resolve(Home), { ssr: false });
+export default page
