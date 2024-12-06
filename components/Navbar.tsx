1a370318b6de2c516cc47295083ec303f1d4741e
Arin7712 <aringawande7712@gmail.com>
2024-12-06 23:55:22 +0530

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,6 @@ import Menu from "@mui/icons-material/Menu";
 import Mail from "@mui/icons-material/Mail";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import {navItems} from '@/constants'
-import { Link as ScrollLink, Element } from "react-scroll";
 import Link from "next/link";
 
 
@@ -53,12 +52,18 @@ const Navbar = () => {
       </div>
 
       <div className="block md:hidden">
-        <section className="flex items-center border-b-[1px] border-gray-100 p-4 w-full justify-between">
+        <section className="flex z-50 items-center border-b-[1px] border-gray-100 p-4 w-full justify-between">
           <div>
             <h2 className="text-md font-bold">AI Sonnet.</h2>
           </div>
 
           <div>
+      <button className="rounded-2xl bg-[#9c6ef7] px-4 py-2 text-sm text-white font-medium">
+              Book a call
+            </button>
+      </div>
+
+          {/*<div className="z-50">
             {menuOpen ? (
               <Close
                 style={{ fontSize: 20, color: "gray" }}
@@ -70,14 +75,13 @@ const Navbar = () => {
                 onClick={toggleMenu}
               />
             )}
-          </div>
-
-          <div
+          </div>*/}
+          {/*<div
             className={`absolute top-16 left-0 w-full h-scree overflow-hidden rounded-md transform transition-all duration-300 ${
               menuOpen ? "opacity-100 translate-y-0 bg-white" : "opacity-0 translate-y-4"
             }`}
           >
-            <div className="bg-[#fafafa] flex flex-col items-start gap-4 p-3">
+            <div className="bg-[#fafafa] flex flex-col items-start gap-4 p-3 z-50">
               <div className="inline-block">
                 <Link href="https://www.google.com/maps/place/18%C2%B028'06.8%22N+73%C2%B051'56.6%22E/@18.468568,73.865709,20z/data=!4m4!3m3!8m2!3d18.4685679!4d73.8657093?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D">
                   <div className="text-gray-500 flex items-center text-sm gap-2">
@@ -98,8 +102,8 @@ const Navbar = () => {
               </div>
             </div>
 
-            <div className="flex flex-col p-3 mt-6">
-              <div>
+            <div className="flex bg-white flex-col p-3 mt-6">
+              <div className="z-50 ">
               <ul className="flex flex-col text-md text-black font-normal gap-3">
               {navItems.map((item, index) => (
                   <li key={index} className="hover:text-gray-900 hover:cursor-pointer transition-all duration-300">{item.title}</li>
@@ -110,13 +114,12 @@ const Navbar = () => {
             <button className="rounded-2xl bg-[#f4f4f5] w-full px-4 py-2 text-md text-black font-medium">
               Our Work
             </button>
-
             <button className="rounded-2xl bg-[#9c6ef7] w-full px-4 py-2 text-md text-white font-medium">
               Book a call
             </button>
           </div>
             </div>
-          </div>
+          </div>*/}
         </section>
       </div>
     </div>
